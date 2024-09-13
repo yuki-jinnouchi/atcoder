0655b985acb1e2cadbcd946cd1300fe108f17cbc
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-09-13 17:36:48 +0900

    add abc238 B

---
+++ abc238/B/main.cpp
@@ -0,0 +1,76 @@
+/**
+ * author:  Jini
+ * sampled by
+ *  https://gist.github.com/yoshihikosuzuki/bbf67915b0920954eb069be3c49b924d
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using sts = set<string>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* main section */
+void solve(ll N, vll A){
+	vll B(N + 1);
+	B[0] = 0;
+	rep(i, N) {
+		rep(k, i + 1){
+			B[k] += A[i];
+			if (B[k] >= 360) {
+				B[k] -= 360;
+			}
+		}
+		B[i + 1] = 0;
+		// rep(k, i + 2) {
+		// 	cout << B[k] << " ";
+		// }
+		// cout << endl;
+	}
+	sort(all(B));
+
+	ll max_degree = 0;
+	rep(i, N + 1) {
+		if (i < N)
+			max_degree = max(max_degree, B[i + 1] - B[i]);
+		else
+			max_degree = max(max_degree, 360 - B[i]);
+	}
+	cout << max_degree << endl;
+}
+
+int main(){
+	ll N;
+	cin >> N;
+	vll A(N);
+	rep(i, N) {
+		cin >> A[i];
+	}
+	solve(N, A);
+	return 0;
+}
