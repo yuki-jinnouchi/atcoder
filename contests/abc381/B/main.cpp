f074494a309f7db9f90b2d58484bbea91d119235
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-12-12 23:58:57 +0900

    add abc381 B

---
+++ contests/abc381/B/main.cpp
@@ -0,0 +1,66 @@
+/**
+ * author:  Jini
+ * sampled by
+ *  https://gist.github.com/yoshihikosuzuki/bbf67915b0920954eb069be3c49b924d
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using sts = set<string>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* main section */
+bool check(string S){
+    if (S.size() % 2 != 0)
+        return false;
+    int len = S.size() / 2;
+    set<char> st;
+    rep(i, len){
+        if (S[2 * i + 1] != S[2 * i])
+            return false;
+        if (st.count(S[2 * i]))
+            return false;
+        st.insert(S[2 * i + 1]);
+    }
+    return true;
+}
+
+void solve(string S){
+    if (check(S))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+int main(){
+    string S;
+	cin >> S;
+    solve(S);
+    return 0;
+}
