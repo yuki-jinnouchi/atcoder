b9f5a60adfebd5c83eb463ad17832d35588cc418
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2025-01-04 19:52:12 +0900

    add abc384 C

---
+++ contests/abc384/C/main.cpp
@@ -0,0 +1,77 @@
+/**
+ * author:  Jini
+ * sampled by
+ *  https://gist.github.com/yoshihikosuzuki/bbf67915b0920954eb069be3c49b924d
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using sts = set<string>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* main section */
+void solve(ll a, ll b, ll c, ll d, ll e){
+	vector<pair<string, ll>> v;
+	vs name = {"A", "B", "C", "D", "E",
+		"AB", "AC", "AD", "AE", "BC", "BD", "BE", "CD", "CE", "DE",
+		"ABC", "ABD", "ABE", "ACD", "ACE", "ADE", "BCD", "BCE", "BDE", "CDE",
+		"ABCD", "ABCE", "ABDE", "ACDE", "BCDE",
+		"ABCDE"};
+	ll score;
+	rep(i, name.size()){
+		score = 0;
+		rep(j, name[i].size()){
+			if (name[i][j] == 'A'){
+				score += a;
+			} else if (name[i][j] == 'B'){
+				score += b;
+			} else if (name[i][j] == 'C'){
+				score += c;
+			} else if (name[i][j] == 'D'){
+				score += d;
+			} else if (name[i][j] == 'E'){
+				score += e;
+			}
+		}
+		v.pb({name[i], score});
+	}
+	sort(v.begin(), v.end(), [](pair<string, ll> a, pair<string, ll> b){
+		return a.second > b.second || (a.second == b.second && a.first < b.first);
+	});
+	rep(i, v.size()){
+		cout << v[i].first << endl;
+	}
+}
+
+int main(){
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+    solve(a, b, c, d, e);
+    return 0;
+}
