03b363a3d5eb057e165f5d23aa1d3e4302f4cdaf
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2025-04-21 14:16:41 +0900

    add abc402 A/B

---
+++ contests/abc402/B/main.cpp
@@ -0,0 +1,65 @@
+/**
+ * author:  Jini
+ * sampled by
+ *  https://gist.github.com/yoshihikosuzuki/bbf67915b0920954eb069be3c49b924d
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using sts = set<string>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* main section */
+void solve(vvi query){
+    vi A;
+    rep(i, query.size()){
+        if(query[i][0] == 2){
+            cout << A[0] << endl;
+            A.erase(A.begin());
+        }
+        if (query[i][0] == 1){
+            A.push_back(query[i][1]);
+        }
+    }
+}
+
+int main(){
+    int Q, a, X;
+    cin >> Q;
+    vvi query(Q, vi(2, 0));
+    rep(i, Q){
+        cin >> a;
+        if(a == 1)
+            cin >> X;
+        query[i][0] = a;
+        query[i][1] = X;
+    }
+    solve(query);
+    return 0;
+}
